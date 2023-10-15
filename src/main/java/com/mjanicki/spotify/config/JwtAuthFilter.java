2a2e2ae46bb113c3385b006875f57fa6432aa0f9
Minto107 <minto.j2000@gmail.com>
2023-10-15 17:51:30 +0200

    auth

---
+++ src/main/java/com/mjanicki/spotify/config/JwtAuthFilter.java
@@ -0,0 +1,50 @@
+package com.mjanicki.spotify.config;
+
+import com.mjanicki.spotify.service.UserService;
+import com.mjanicki.spotify.service.security.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String header = request.getHeader("Authorization");
+        final String jwt;
+        final String email;
+        if (!StringUtils.hasLength(header) || !StringUtils.startsWithIgnoreCase(header, "Bearer ")) {
+            filterChain.doFilter(request, response);
+            return ;
+        }
+        jwt = header.substring(7);
+        email = jwtService.extractUsername(jwt);
+
+        if (!StringUtils.hasLength(email) && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userService.userDetailsService().loadUserByUsername(email);
+            if (jwtService.isTokenValid(email, userDetails)) {
+                SecurityContext context = SecurityContextHolder.createEmptyContext();
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                context.setAuthentication(authToken);
+                SecurityContextHolder.setContext(context);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}

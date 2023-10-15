905419fd02ea3bd91156eee047806f22019a51d2
Minto107 <minto.j2000@gmail.com>
2023-10-15 20:49:11 +0200

    further auth conf

--- src/main/java/com/mjanicki/spotify/config/SecurityConfig.java
+++ src/main/java/com/mjanicki/spotify/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -30,7 +31,7 @@ public class SecurityConfig {
         http.csrf(httpSecurityCsrfConfigurer -> {
             try {
                 httpSecurityCsrfConfigurer.disable()
-                        .authorizeHttpRequests(req -> req.requestMatchers("/api/auth/**")
+                        .authorizeHttpRequests(req -> req.requestMatchers(new AntPathRequestMatcher("/api/auth/**"))
                                 .permitAll().anyRequest().authenticated())
                         .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                         .authenticationProvider(authenticationProvider()).addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
---
+++ src/main/java/com/mjanicki/spotify/controller/security/AuthController.java
@@ -0,0 +1,27 @@
+package com.mjanicki.spotify.controller.security;
+
+import com.mjanicki.spotify.dao.security.LoginRequest;
+import com.mjanicki.spotify.dao.security.RegisterRequest;
+import com.mjanicki.spotify.service.security.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(service.login(request));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(service.register(request));
+    }
+}
+
